f16c5564c82a5f17bae5fd3d842a8b05df173b49
Eashaan Katiyar <eashaank@gmail.com>
2020-07-08 01:09:20 -0700

    Started work on flow, done for tonight

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import spotipy
 import os
 import common
+import time
 
-# common.set_vars()
+# import mock
+# mock.set_vars()
 
 auth_manager = common.environ_auth_manager(common.scopes_list)
 token = common.get_token(auth_manager)
 
 sp = spotipy.Spotify(auth=token["access_token"])
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+cached_song = None
 
+while(True):
+    results = sp.currently_playing()
+    if False and (results == None or not results["is_playing"]):
+        time.sleep(5 * 60)
+        cached_current = results
+        continue
+
+    if cached_song == None or results["item"]["id"] == cached_song["item"]["id"]:
+        time.sleep(5)
+        cached_current = results
+        continue
+
+    song_id = results["item"]["id"]
+
+    in_saved = sp.current_user_saved_tracks_contains([song_id])
+
+    print(in_saved)
+
+    cached_song = results
