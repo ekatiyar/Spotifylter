30cbf51f65d03d794da21b5db4ba9e53aa5e17a0
Eashaan Katiyar <eashaank@gmail.com>
2020-07-13 11:13:19 -0700

    Added function to delete entry by username

--- db.py
+++ db.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
 import models
 
-# import mock
-# mock.set_vars()
+import mock
+mock.set_vars()
 
 engine = create_engine(getenv("DATABASE_URL"))
 
@@ -15,6 +15,14 @@ Scoped_Session = scoped_session(Session_Factory)
 def recreate_database() -> None:
     models.Base.metadata.drop_all(engine)
     models.Base.metadata.create_all(engine)
+
+
+def delete_entry(username: str) -> None:
+    s = Session_Factory()
+    s.query(models.Users).filter_by(
+        username=username).delete(synchronize_session=False)
+    s.commit()
+    s.close()
 
 
 if __name__ == "__main__":
